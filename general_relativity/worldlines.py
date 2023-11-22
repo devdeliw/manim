cfd1a11a6d8c5f45de55037ad7b83f210a1f8145
Deval Deliwala <devaldeliwala@berkeley.edu>
2023-11-21 18:14:18 -0700

    general relativity update

--- general_relativity/worldlines.py
+++ general_relativity/worldlines.py
@@ -895,15 +895,520 @@ class ProperLength(ThreeDScene):
 
         self.stop_ambient_camera_rotation()
 
+
+class MetricTensor(ThreeDScene):
+    def construct(self):
+
+        self.set_camera_orientation(
+            phi = 70*DEGREES,
+            theta = -35*DEGREES, 
+            distance = 5
+        )
+
+        axes = ThreeDAxes(
+            x_range = (-5, 5, 5), 
+            y_range = (-5, 5, 5), 
+            z_range = (-5, 5, 5), 
+            x_length = 7, 
+            y_length = 7, 
+            z_length = 7,
+            axis_config={'tip_shape': StealthTip}
+        )
+        
+        v1 = Arrow3D(
+            start = np.array([-2, -2, -2]),
+            end = np.array([2, 2, 2]),
+            resolution = 8, 
+            color = WHITE, 
+        ).set_color_by_gradient(TEAL_B, MAROON_A)
+
+        v2 = Arrow3D(
+            start = np.array([2, 2, 2]),
+            end = np.array([2, 2, -2]),
+            resolution = 8, 
+            color = WHITE, 
+        ).set_color_by_gradient(RED_A, PURPLE_B)
+
+        v1l = MathTex(r"\vec{v}_1").move_to([-0.4, 0.6, 0])
+        v1l2 = v1l.copy()
+        v2l = MathTex(r"\vec{v}_2").move_to([3.4, -0.2, 0])
+        v1ll = MathTex(r"||\vec{v}_1||").move_to([-0.4, 0.6, 0])
+
+        v1line = Line(start =  [-2, -2, -1.9], end = [2, 2, 2.1])
+
+        self.add_fixed_in_frame_mobjects(v1l, v1l2, v2l, v1ll)
+        self.remove(v1l, v2l, v1l2, v1ll)
+
+        angle = ArcBetweenPoints([1.4,1.4,1.4], [2,2,1.2], radius = 0.5)
+
+        self.play(Write(v1), run_time = 2)
+        self.play(FadeIn(v1l), run_time = 1)
+
+        self.wait(2)
+
+        self.play(Write(v1line), Unwrite(v1l), Write(v1ll), run_time = 2)
+
+        self.wait(4)
+
+        self.play(FadeOut(v1line), Unwrite(v1ll), Write(v1l2), run_time = 2)
+
+        self.wait()
+
+        self.play(Write(v2), run_time = 2)
+        self.play(Write(v2l), run_time = 1)
+
+        self.wait(2)
+
+        self.play(Unwrite(VGroup(v1l2, v2l)))
+
+        self.wait()
+
+        self.play(Write(angle), run_time = 1)
+
+        self.begin_ambient_camera_rotation(rate = 0.3)
+
+        self.play(Unwrite(angle), Unwrite(VGroup(v1, v2)), run_time = 2)
+
+        self.wait(1)
+
+        cube = Cube(side_length=5, fill_opacity=0.7, fill_color=BLUE).set_color([TEAL_B, PINK, YELLOW])
+        cube3 = cube.copy().scale(0.6).set_color([PINK, YELLOW])
+        cube2 = cube.copy().scale(0.8).set_color([MAROON_A, TEAL_B])
+
+        self.play(FadeIn(cube), Write(axes), run_time = 2)
+
+        self.wait(2)
+
+        self.play(FadeIn(cube2))
+        self.play(FadeIn(cube3))
+
+        self.wait(4)
+
+        self.play(Unwrite(VGroup(cube, cube2, cube3, axes)), run_time = 4)
+        self.wait()
+
+
+class MetricTensor2(Scene):
+    def construct(self):
+
+        minkowski_metric = MathTex(r" g_\text{minkowski} = \begin{bmatrix} -1 & 0 & 0 & 0 \\ 0 & 1 & 0 & 0 \\ 0 & 0 & 1 & 0 \\ 0 & 0 & 0 & 1 \end{bmatrix}"
+        )
+
+        
+        schwarzschild_metric = MathTex(r" g_\text{schwarzschild} = \begin{bmatrix} -\left(1 - \frac{2GM}{rc^2}\
+                \right)  & 0 & 0 & 0 \\ 0 & \left(1 - \frac{2GM}{rc^2}\right)^{-1} & 0 & 0 \\ \
+                0 & 0 & r^2 & 0 \\ 0 & 0 & 0 & r^2\sin^2 \theta \end{bmatrix}").scale(0.75)
+
+        
+        metric_tensor = Tex(r" Metric Tensor ").scale(1.5).set_color([TEAL_B, MAROON_A]).to_edge(DOWN)
+
+        self.play(DrawBorderThenFill(minkowski_metric.move_to([0, 2, 0])), run_time = 2)
+
+        formula = MathTex(r" (\Delta s)^2 = g_{\mu \nu} \Delta x^\mu \Delta x^\nu ").move_to([0, -1, 0])
+        formula_rect = SurroundingRectangle(formula).set_color([TEAL_B, PINK, YELLOW]).move_to([0, -1, 0])
+
+        self.wait(2)
+
+        self.play(DrawBorderThenFill(VGroup(formula, formula_rect)), run_time = 2)
+
+        self.wait(2)
+
+        self.play(DrawBorderThenFill(metric_tensor), run_time = 3)
+
+        self.wait(5)
+
+        self.play(Unwrite(VGroup(formula, formula_rect, metric_tensor)), run_time = 3)
+        self.wait(1)
+
+        self.play(minkowski_metric.animate.scale(0.75))
+
+        self.play(Write(schwarzschild_metric), run_time = 3)
+
+        self.wait() 
+
+        self.play(minkowski_metric.animate.to_edge(LEFT), schwarzschild_metric.animate.to_edge(LEFT), run_time = 2)
+
+        self.wait()
+
+        metric = VGroup(minkowski_metric, schwarzschild_metric)
+        metric_rect = SurroundingRectangle(metric).set_color([TEAL_B, MAROON_A]).scale(0.75)
+
+        metrics = VGroup(metric, metric_rect)
+
+        self.play(Write(metric_rect.to_edge(UL)), metric.animate.scale(0.75).to_edge(UL))
+
+        self.wait(20)
+
+        self.play(Unwrite(VGroup(metric_rect, metric)), run_time = 4)
+
+
+class MetricGeometry(LinearTransformationScene):
+    def __init__(self):
+        LinearTransformationScene.__init__(
+            self,
+            show_coordinates = True,
+            leave_ghost_vectors = True,
+            show_basis_vectors = True
+        )
+    def construct(self):
+        v12 = Arrow([2,2,0], [-2, 3, 0]).set_color([TEAL_B, YELLOW, PINK])
+        v122 = Arrow([4, 0.5, 0], [1, 2, 0]).set_color([PINK, YELLOW, TEAL_B])
+
+        self.add_vector([2,2], TEAL_B)
+        self.add_vector([-2,3], MAROON_A)
+        self.add_vector(v12)
+
+
+        self.wait(2)
+
+        matrix = [[-0.5, 1], [0.5, 0.5]]
+        self.moving_mobjects = []
+
+        self.apply_matrix(matrix)
+        self.wait()
+
+        self.play(Transform(v12, v122))
+
+        self.wait(3)
+
+
+class MinkowskiReview(ThreeDScene):
+    def construct(self):
+
+        self.set_camera_orientation(
+            phi = 65*DEGREES,
+            theta = 30*DEGREES, 
+            distance = 5
+        )
+
+        axes = ThreeDAxes(
+            x_range = (-7, 7, 7), 
+            y_range = (-7, 7, 7), 
+            z_range = (0, 7, 7), 
+            x_length = 6, 
+            y_length = 6, 
+            z_length = 6.2,
+            axis_config={'tip_shape': StealthTip}
+        ).move_to([0, 0, -0.2])
+        labels = axes.get_axis_labels(
+                    Tex(r"SPACE").scale(0.75), Tex(r"SPACE").scale(0.75), Tex(r"$t$")
+        )
+
+        cubes = VGroup()
+        for i in range(-2, 2): 
+            for j in range(-2, 2): 
+                for k in range(-2, 2):
+                    cube = Cube(side_length = 1, 
+                                fill_opacity = 0.3, 
+                                stroke_width = 0.3
+                    ).move_to([i, j, k]).set_color([GRAY_C]) 
+
+                    cubes.add(cube)
+
+        self.begin_ambient_camera_rotation(rate = 0.1)
+
+        self.play(DrawBorderThenFill(cubes), run_time = 2)
+
+        self.wait(20)
+
+        self.play(Unwrite(cubes), run_time = 4)
+        self.wait()
+
+        """
+        title = Tex(r"Minkowski Space").to_edge(UL).set_color([TEAL_B, MAROON_A])
+        self.add_fixed_in_frame_mobjects(title)
+        self.remove(title)
+
+        metric = MathTex(r" g_{\mu \nu} = \begin{bmatrix} -1 & 0 & 0 & 0 \\ 0 & 1 & 0 & 0 \\ 0 & 0 & 1 & 0 \\ 0 & 0 & 0 & 1 \end{bmatrix} ")
+
+        equation = MathTex(r" (\Delta s)^2 = g_{\mu\nu} \Delta x^\mu \Delta x^\nu ")
+        self.add_fixed_in_frame_mobjects(metric, equation)
+        self.remove(metric, equation)
+
+        self.play(Write(title), run_time = 2)
+
+        self.wait()
+
+        self.begin_ambient_camera_rotation(rate = 0.1)
+
+        self.play(DrawBorderThenFill(cubes), run_time = 3)
+
+        self.wait(6)
+
+        metrics = VGroup(metric, equation).arrange(RIGHT, buff = 2).scale(0.75)
+        rect = SurroundingRectangle(metrics, buff = 0.3).set_color([TEAL_B, PINK, YELLOW])
+
+        self.add_fixed_in_frame_mobjects(rect)
+        self.remove(rect)
+
+        self.play(cubes.animate.to_edge(UR))
+
+        self.wait()
+        
+        self.play(Write(metric), run_time = 2)
+        self.wait()
+        self.play(Write(equation), run_time = 2)
+        self.play(FadeIn(rect), run_time = 2)
+        self.wait()
+        """
+
+class MinkowskiReview2(Scene):
+    def construct(self):
+        title = Tex(r" Minkowski Space ").scale(1.5).set_color([TEAL_B, MAROON_A]).to_edge(UL)
+
+        metric = MathTex(r" g_{\mu\nu} = \begin{bmatrix} -1 & 0 & 0 & 0 \\ 0 & 1 & 0 & 0 \\ 0 & 0 & 1 & 0 \\ 0 & 0 & 0 & 1 \end{bmatrix} ")
+        equation = MathTex(r" (\Delta s^2) = g_{\mu\nu} \Delta x^\mu \Delta x^\nu ")
+
+        metrics = VGroup(metric, equation).arrange(RIGHT, buff = 1.5).scale(0.9).to_edge(DOWN).shift(UP*1)
+        metric_rect = SurroundingRectangle(metrics, buff = 0.3).set_color([TEAL_B, PINK, YELLOW])
+
+        self.play(Write(title), run_time = 2)
+        self.wait(5)
+
+        self.play(Write(metric), run_time = 2)
+        self.wait(2)
+        self.play(Write(equation), run_time = 2)
+        self.play(Write(metric_rect))
+        
+        self.wait(5)
+
+        self.play(Unwrite(VGroup(metrics, metric_rect), run_time = 4))
+        self.wait()
+
+
+class Review(ThreeDScene):
+    def construct(self):
+        
+        title = Tex(r"Review").scale(2)
+
+        self.set_camera_orientation(
+            phi = 60*DEGREES,
+            theta = 80*DEGREES,
+            distance = 10
+        )
+
+
+        self.add_fixed_in_frame_mobjects(title)
+        self.remove(title)
+
+        self.play(Write(title))
+        self.wait()
+
+        z1 = Surface(
+           lambda u, v: np.array([u, v, v * 0.3*np.sin(u) - u *0.3*np.cos(v)]),
+           resolution = (16, 32),
+           v_range = [-2, 2],
+           u_range = [-2, 2],
+           fill_opacity = 0.1
+        ).set_opacity(0.2)
+
+        z1.set_color_by_gradient(TEAL, MAROON, YELLOW)
+
+        z2 = Surface(
+           lambda u, v: np.array([u, v, u * 4*math.e**(-u**2 - v**2)]),
+           resolution = (16, 32),
+           v_range = [-2, 2],
+           u_range = [-2, 2],
+           fill_opacity = 0.1
+        ).set_opacity(0.2)
+
+        z2.set_color_by_gradient(MAROON, YELLOW, TEAL)
+
+        z3 = Surface(
+           lambda u, v: np.array([u, v, v * -0.3*np.sin(u) - u * -0.3*np.cos(v)]),
+           resolution = (16, 32),
+           v_range = [-2, 2],
+           u_range = [-2, 2],
+           fill_opacity = 0.1
+        ).set_opacity(0.2)
+
+        z3.set_color_by_gradient(YELLOW, MAROON, TEAL)
+
+        z4 = Surface(
+           lambda u, v: np.array([u, v, u * -4*math.e**(-u**2 - v**2)]),
+           resolution = (16, 32),
+           v_range = [-2, 2],
+           u_range = [-2, 2],
+           fill_opacity = 0.1
+        ).set_opacity(0.2)
+
+        z4.set_color_by_gradient(TEAL, YELLOW, MAROON)
+
+        sheet = Surface(
+           lambda u, v: np.array([u, v, 0]),
+           resolution = (1, 1),
+           v_range = [-4, 4],
+           u_range = [-2, 2],
+           fill_opacity = 0.1
+        ).set_opacity(0.2).set_color([TEAL, MAROON, YELLOW])
+
+        z11 = z1.copy()
+        z12 = z1.copy()
+
+        z21 = z2.copy()
+        z22 = z2.copy()
+
+        z31 = z3.copy()
+
+        z41 = z4.copy()
+
+        self.play(Write(z1), Write(z2), run_time = 1.5)
+        self.play(ReplacementTransform(z1, z3), ReplacementTransform(z2, z4), run_time = 2)
+        self.play(ReplacementTransform(z3, z11), ReplacementTransform(z4, z21), run_time = 2)
+
+        self.play(Unwrite(VGroup(title, z11, z21)), run_time = 2)
+
+        self.wait()
+
+class LightConeReview(ThreeDScene):
+    def construct(self): 
+
+        self.set_camera_orientation(
+            phi = 90*DEGREES,
+            theta = 0*DEGREES, 
+            distance = 5
+        )
+        
+        axes = ThreeDAxes(
+            x_range = (-5, 5, 5), 
+            y_range =(-5 , 5, 5), 
+            z_range = (-5, 5, 5), 
+            x_length = 7, 
+            y_length = 7, 
+            z_length = 7,
+            axis_config={'tip_shape': StealthTip}
+        )
+        labels = axes.get_axis_labels(
+                    Tex(r"$x$"), Tex(r"$x$"), Tex(r"")
+                )
+
+        lightcone_up = Cone(2, 2, [0,0,-1], resolution = 20, fill_opacity = 0.3).set_color([YELLOW])
+        lightcone_down = Cone(2, 2, [0,0,1], resolution = 20, fill_opacity = 0.3).set_color([YELLOW])
+        lightcone = VGroup(lightcone_up, lightcone_down).set_color([TEAL_B])
+
+        x = MathTex(r"x").move_to([3.3, -0.2, 0])
+        t = MathTex(r"t").move_to([0.2, 3.3, 0])
+        self.add_fixed_in_frame_mobjects(x,t)
+        self.remove(x, t)
+
+        axis = VGroup(axes, labels)
+
+
+        title = Tex(r" Light Cone ").scale(1.5).to_edge(UL).set_color([PINK, YELLOW])
+        timelike = Tex(r" timelike ").move_to([-2, 0.5, 0]).set_color([RED_C, PURPLE_A]).scale(0.9)
+        spacelike = Tex(r" spacelike ").move_to([2, -0.5, 0]).set_color([BLUE_B, GRAY_C]).scale(0.9)
+        null = Tex(r" null ").move_to([1.5, 0.3, 0]).scale(0.9).set_color([TEAL_B, MAROON_A, PINK, YELLOW])
+
+        self.add_fixed_in_frame_mobjects(title, timelike, spacelike, null)
+        self.remove(title, timelike, spacelike, null)
+
+        text = VGroup(title, timelike, spacelike, null)
+
+        lightcone_time = lightcone_up.copy().set_opacity(0.9)
+        
+        lightcone_null = VGroup(lightcone_up, lightcone_down).set_opacity(0.1).set_color([YELLOW])
+
+        self.play(Write(title), run_time = 2)
+
+        self.wait(1)
+
+        self.play(Write(VGroup(lightcone, axis)), Write(VGroup(x, t)), run_time = 5)
+
+        self.wait(8)
+
+        self.play(Write(timelike), FadeIn(lightcone_time), run_time = 2)
+
+        self.wait(3)
+
+        self.play(FadeOut(lightcone_time), Unwrite(timelike), run_time = 2)
+
+        spacelike_space = VGroup(lightcone.copy().set_color([BLACK]).set_opacity(1), 
+                                 Surface(
+                                     lambda u, v: np.array([0, u, v]),
+                                     u_range = [-7, 7],
+                                     v_range = [-7, 7], 
+                                     resolution = [4, 8], 
+                                 ).set_color([YELLOW])
+        )
+                                 
+
+        self.wait(3)
+
+        self.play(Write(spacelike), run_time = 2)
+        self.play(FadeIn(spacelike_space), run_time = 2)
+        self.play(FadeOut(spacelike_space), run_time = 2)
+        self.wait()
+        self.play(Unwrite(spacelike), run_time = 2)
+    
+        self.wait(2)
+
+        null_space = VGroup(Line([-4, -4, -4], [4, 4, 4]), Line([-4, 4, -4], [4, -4, 4]), 
+                            Line([4, -4, -4], [-4, 4, 4]), Line([4, 4, -4], [-4, -4, 4])).set_color([TEAL_B])
+        
+        self.play(Write(null_space), Write(null), run_time = 4)
+
+        self.wait(4)
+
+        self.play(Unwrite(VGroup(null, null_space)), run_time = 4)
+        
+        self.wait(4)
+
+        self.play(Unwrite(VGroup(lightcone, axis, t, x, title)), run_time = 5)
+
+        self.wait()
+
+
+class EinsteinFieldEquation(Scene):
+    def construct(self):
+        
+        equation = MathTex(r"R_{\mu\nu} - \frac{1}{2} R", r"g_{\mu\nu}", r" + \Lambda", r"g_{\mu\nu}", r"= \kappa T_{\mu\nu}").scale(1.5).set_color([TEAL_B, MAROON_A])
+
+        self.play(DrawBorderThenFill(equation), run_time = 4)
+
+        self.wait(5)
+
+        self.play(equation[1].animate.set_color([YELLOW]), equation[3].animate.set_color([YELLOW]), run_time = 4)
+
+        self.wait(10)
+
+        self.play(Uncreate(equation), run_time = 3)
+
+        self.wait()
+
+class watermark(Scene):
+    def construct(self):
+        t = Tex(r"youtube.com/@devdeliw").scale(0.75).to_edge(DL)
+        self.add(t)
+        self.wait(10)
+
+
+
+
+
+        
+        
         
         
 
 
 
+
         
 
 
+
+
+
         
+        
+        
+        
+        
+        
+
+
+
+ 
                     
         
 
